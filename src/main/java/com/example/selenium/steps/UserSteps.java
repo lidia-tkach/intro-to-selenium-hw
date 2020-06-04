ada9507feb4231ec1014aa9c3524e432d0f28f7b
Lidia Tkach <tkach.lidia61@gmail.com>
2020-06-05 02:56:50 +0300

    Selenium homework completed

--- src/main/java/com/example/selenium/steps/UserSteps.java
+++ src/main/java/com/example/selenium/steps/UserSteps.java
@@ -1,6 +1,10 @@
 package com.example.selenium.steps;
 
+import com.example.selenium.pages.LoginPage;
+import com.example.selenium.pages.MainPage;
 import org.openqa.selenium.WebDriver;
+
+import static org.testng.Assert.assertEquals;
 
 public class UserSteps {
     private WebDriver driver;
@@ -10,14 +14,18 @@ public class UserSteps {
     }
 
     public com.example.selenium.steps.UserSteps login(String username, String password) {
-        // TODO initialize LoginPage, call setUsername, setPassword, and click login methods
-
-        //  TODO assert title
+        LoginPage loginPage = new LoginPage(driver);
+        MainPage mainPage = new MainPage(driver);
+        loginPage.setUsername(username);
+        loginPage.setPassword(password);
+        loginPage.clickLogin();
+        assertEquals(mainPage.getTitle(), "Secure Area", "Login failed");
         return this;
     }
 
     public UserSteps logout() {
-        // TODO call logout method
+        MainPage mainPage = new MainPage(driver);
+        mainPage.clickLogout();
         return this;
     }
 }
